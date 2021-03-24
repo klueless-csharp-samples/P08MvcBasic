a9ad071473eab40907bb663c50502f9395946b6d
klueless-io <david@ideasmen.com.au>
2021-03-24 19:01:17 +1100

    add current project

---
+++ Models/Person.cs
@@ -0,0 +1,11 @@
+namespace P08MvcBasic.Models
+{
+  public class Person
+  {
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public int Age { get; set; }
+  }
+}
