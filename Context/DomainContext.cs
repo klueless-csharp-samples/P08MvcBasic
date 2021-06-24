eaa3c6098000e505c1e0afee0fcce19b4c8ec9ea
David Cruwys <david@ideasmen.com.au>
2021-06-24 14:59:36 +1000

    update

---
+++ Context/DomainContext.cs
@@ -0,0 +1,19 @@
+﻿namespace P08MvcBasic.Context
+{
+  using Microsoft.EntityFrameworkCore;
+  using P08MvcBasic.Models;
+
+  public class DomainContext : DbContext
+  {
+    public DbSet<Product> Products { get; set; }
+
+    public DbSet<Sale> Sales { get; set; }
+
+    public DbSet<SaleLineItem> SaleLineItems { get; set; }
+
+    public DbSet<Store> Stores { get; set; }
+
+    public DbSet<Terminal> Terminals { get; set; }
+
+  }
+}
---
+++ Models/Product.designer.cs
@@ -0,0 +1,18 @@
+namespace P08MvcBasic.Models
+{
+  using System;
+  using System.Collections.Generic;
+
+  public partial class Product
+  {
+    public int Id { get; set; }
+
+    // Common fields
+    public string Name { get; set; }
+
+    public decimal Price { get; set; }
+
+
+
+  }
+}
